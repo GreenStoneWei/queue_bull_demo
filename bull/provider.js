a238e9ec338105d06cd19e64fd100b0ac7b331a1
魏士弘 <stone.wei@wemoscooter.com>
2020-02-23 00:30:37 +0800

    almost done

--- bull/provider.js
+++ bull/provider.js
@@ -1,9 +1,7 @@
-const config = require('config')
-// const redisConfig = config.get('redis')
 const Queue = require('bull')
 
 const defaultJobOpts = {
-  delay: 30 * 1000, // seconds
+  delay: 20 * 1000, // seconds
   attempts: 3,
   removeOnComplete: true,
   removeOnFail: false
@@ -38,7 +36,7 @@ exports.add = async jobId => {
   let job = await appQueue.getJob(jobId)
   if (!job) {
     /** Add a new job to queue */
-    job = await appQueue.add({ jobId }, { ...defaultJobOpts, jobId }) // Default delay for 65s, jobId could not be override
+    job = await appQueue.add({ jobId }, { ...defaultJobOpts, jobId }) // Default delay for 20s, jobId could not be override
   } else {
     /** If the job is existed, retry */
     await job.retry().catch(error => console.log(error.message))
